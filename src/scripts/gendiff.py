6285b410856b528c505c2fc4689ac6a6b0a9e9d3
lingalo <bokova.nast@yandex.ru>
2023-04-22 00:27:33 +0300

    add optional argument "format"

--- src/scripts/gendiff.py
+++ src/scripts/gendiff.py
@@ -8,6 +8,8 @@ def gendiff():
         )
     parser.add_argument('first_file')
     parser.add_argument('second_file')
+
+    parser.add_argument('-f', '--format', help='set format of output')
     return parser.parse_args()
 
 

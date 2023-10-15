fb563bf85d1c2ce7e57b5bdf98b7ce0f1e6ef274
lingalo <bokova.nast@yandex.ru>
2023-10-16 01:14:34 +0300

    Modifited gendiff.py, __init__.py, files.json moved to test/fixture/

---
+++ gendiff/__init__.py
@@ -0,0 +1,7 @@
+from gendiff.scripts.gendiff import generate_diff, to_json_value
+
+
+__all__ = (
+    'generate_diff',
+    'to_json_value'
+    )
--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -8,7 +8,7 @@ FILE1 = '  - '
 FILE2 = '  + '
 
 
-def to_jsonf(value):
+def to_json_value(value):
     if value is False:
         return 'false'
     if value is True:
@@ -25,8 +25,8 @@ def generate_diff(file_path1, file_path2):
 
     result = ['{']
     for key in s_data:
-        value1 = to_jsonf(data1.get(key, 'not_value'))
-        value2 = to_jsonf(data2.get(key, 'not_value'))
+        value1 = to_json_value(data1.get(key, 'not_value'))
+        value2 = to_json_value(data2.get(key, 'not_value'))
 
         if value1 == value2:
             result.append(f'{SAME}{key}: {value1}')
@@ -36,15 +36,12 @@ def generate_diff(file_path1, file_path2):
             if value2 != 'not_value':
                 result.append(f'{FILE2}{key}: {value2}')
     result.append('}')
-    result = '\n'.join(result)
-    print(result)
-    return result
+    return '\n'.join(result)
 
 
 def gen():
-    # print('hello')
     parser = argparse.ArgumentParser(
-        prog='gendiff', 
+        prog='gendiff',
         description='Compares two configuration files and shows a difference.'
         )
     parser.add_argument('first_file')
@@ -54,9 +51,9 @@ def gen():
     args = parser.parse_args()
     generate_diff(args.first_file, args.second_file)
 
+
 def main():
     gen()
-    # generate_diff()
 
 
 if __name__ == '__main__':

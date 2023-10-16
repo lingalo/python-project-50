2b584d96d366e9bf44e53b2ce513e5bca6cb377b
lingalo <bokova.nast@yandex.ru>
2023-10-16 16:05:03 +0300

    Changed path in test_gendiff.py

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 from gendiff import generate_diff as gen_dif, to_json_value as to_json
+import pathlib
 
 
-FILE_PATH1 = '/home/furia/python-project-50/tests/fixtures/file1.json'
-FILE_PATH2 = '/home/furia/python-project-50/tests/fixtures/file2.json'
-RIGHT = '/home/furia/python-project-50/tests/fixtures/diff_file1_file2'
+FILE_PATH1 = pathlib.Path("tests", "fixtures", "file1.json")
+FILE_PATH2 = pathlib.Path("tests", "fixtures", "file2.json")
+RIGHT = pathlib.Path("tests", "fixtures", "diff_file1_file2")
 
 
 def test_gen_dif():

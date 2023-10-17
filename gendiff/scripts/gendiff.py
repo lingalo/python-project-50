9710757772789f7405865dee7909634381ab8288
lingalo <bokova.nast@yandex.ru>
2023-10-17 04:41:37 +0300

    Function generate_diff moved to parsing.py, added function file_to_dict

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,45 +1,9 @@
 #!/usr/bin/env python3
+from gendiff.scripts import parsing
 import argparse
-import json
 
 
-SAME = '    '
-FILE1 = '  - '
-FILE2 = '  + '
-
-
-def to_json_value(value):
-    if value is False:
-        return 'false'
-    if value is True:
-        return 'true'
-    if value is None:
-        return 'null'
-    return value
-
-
-def generate_diff(file_path1, file_path2):
-    data1 = json.load(open(file_path1))
-    data2 = json.load(open(file_path2))
-    s_data = sorted(set(data1) | set(data2))
-
-    result = ['{']
-    for key in s_data:
-        value1 = to_json_value(data1.get(key, 'not_value'))
-        value2 = to_json_value(data2.get(key, 'not_value'))
-
-        if value1 == value2:
-            result.append(f'{SAME}{key}: {value1}')
-        else:
-            if value1 != 'not_value':
-                result.append(f'{FILE1}{key}: {value1}')
-            if value2 != 'not_value':
-                result.append(f'{FILE2}{key}: {value2}')
-    result.append('}')
-    return '\n'.join(result)
-
-
-def gen():
+def parse():
     parser = argparse.ArgumentParser(
         prog='gendiff',
         description='Compares two configuration files and shows a difference.'
@@ -49,11 +13,11 @@ def gen():
     parser.add_argument('-f', '--format', help='set format of output')
 
     args = parser.parse_args()
-    generate_diff(args.first_file, args.second_file)
+    print(parsing.generate_diff(args.first_file, args.second_file))
 
 
 def main():
-    gen()
+    parse()
 
 
 if __name__ == '__main__':

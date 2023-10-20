ac45e80f4ab4a4c7be5ddb4c45b157aa3b21e7d6
lingalo <bokova.nast@yandex.ru>
2023-10-21 00:27:59 +0300

    Modified init.py

--- gendiff/__init__.py
+++ gendiff/__init__.py
@@ -1,4 +1,5 @@
-from gendiff.scripts.parsing import generate_diff, to_json_value
+from gendiff.parsing import generate_diff
+from gendiff.to_json_value import to_json_value
 from gendiff.scripts.gendiff import parse
 
 
